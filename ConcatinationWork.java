8a858a602bd445aac74c701767aba94682bcc137
https://github.com/Rajeshwari10Bligar/WhileLoop.git <rajeshwaribaligar@gmail.com>
2019-01-09 17:50:04 +0000

    Concatinationhomwork

---
+++ ConcatinationWork.java
@@ -0,0 +1,17 @@
+package loveProgramming;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class ConcatinationWork {
+    public static void main(String[] args) {
+        String s1 = " The";
+        String s2 = " String";
+        String s3 = " Concatination";
+        String s4 = " Work";
+        String s5 = s1 + s2 + s3 + s4 ;
+        {
+            System.out.println(s5);
+        }
+
+    }
+}
